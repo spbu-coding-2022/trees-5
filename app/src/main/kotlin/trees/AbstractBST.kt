2f3d7e06315636c5b95c4595ee8a7ae8ad6e832a
PavlushaSource <pavlushkin.dima1785@gmail.com>
2023-04-18 21:13:13 +0300

    feat: Fix mergeable feedback, add gradle wrapper, add workflows.

--- app/src/main/kotlin/trees/AbstractBST.kt
+++ app/src/main/kotlin/trees/AbstractBST.kt
@@ -1,10 +1,10 @@
-package etrees
+package trees
 
 abstract class AbstractBST<K : Comparable<K>, Subtree : AbstractBST<K, Subtree>> {
-    protected open var key: K? = null
-    protected open var value: Any? = null
-    protected open var right: Subtree? = null
-    protected open var left: Subtree? = null
+    internal var key: K? = null
+    internal var value: Any? = null
+    internal var right: Subtree? = null
+    internal var left: Subtree? = null
 
     protected fun greatThan(x: K?, y: K?): Boolean = !(x == null || y == null || y >= x)
     protected fun lessThan(x: K?, y: K?): Boolean = !(x == null || y == null || y <= x)
@@ -17,6 +17,13 @@ abstract class AbstractBST<K : Comparable<K>, Subtree : AbstractBST<K, Subtree>>
             currentKey < key -> right?.findByKey(key)
             else -> this.value
         }
+    }
+
+    protected fun copyFields(tree: Subtree) {
+        this.key = tree.key
+        this.value = tree.value
+        this.left = tree.left
+        this.right = tree.right
     }
 
     fun showOrderKeys() {
